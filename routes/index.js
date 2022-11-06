d14ddc073130504ed20a6ac6872549d0fe2fbda8
rahul0168 <rahul.naikmule.facebook@gmail.com>
2022-11-06 10:11:47 +0530

    controller added

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,11 @@ const express = require('express');
 
 
 const router = express.Router();
+const homeController = require('../controllers/home_controller');
 console.log('router is running');
+
+router.get('/',homeController.home);
+
 module.exports = router;
+
+
