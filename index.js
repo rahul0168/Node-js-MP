0d517d69a9fbdeec7cc85dd5ddf45d9aa74c056a
rahul0168 <rahul.naikmule.facebook@gmail.com>
2022-11-06 10:37:32 +0530

    added ejs template engine

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const app = express();
 const port = 8000;
 
 //ue express router
-
 app.use('/', require('./routes'));
+
+//EJS Template
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
+
 
 app.listen(port, function(err){
     if (err){
